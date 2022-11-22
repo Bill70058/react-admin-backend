1fa558f4204cc727b657e42d85611fc112eaa48d
lzr <lzr@email.com>
2022-11-22 22:10:13 +0800

    新增删除菜单接口

--- routes/page.js
+++ routes/page.js
@@ -2,7 +2,7 @@
  * @Author: lzr lzr@email.com
  * @Date: 2022-11-20 14:59:48
  * @LastEditors: lzr lzr@email.com
- * @LastEditTime: 2022-11-21 22:21:42
+ * @LastEditTime: 2022-11-22 22:00:21
  * @FilePath: /react-admin-backend/routes/page.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -71,7 +71,7 @@ router.route('/').get((req, res) => {
           pageMap[item.parentId] = {
             children: [item]
           }
-        } else if (typeof item.id != 'undefined' && typeof item.isLeaf != 'undefined') {
+        } else if (typeof item.id != 'undefined' && typeof item.isLeaf != 'undefined' && item.isLeaf) {
           // 当前节点为父节点，且map中已有children的占位对象，则合并对象
           let tempObj = Object.assign({}, item, pageMap[item.id])
           pageMap[item.id] = tempObj
@@ -93,6 +93,27 @@ router.route('/').get((req, res) => {
         msg: err
       }))
     })
+})
+
+router.route('/').delete((req, res) => {
+  const {
+    id,
+    key
+  } = req.body
+  Page.deleteOne({
+      id: id
+    }).then((info) => {
+      res.json(template.msgTemplate({
+        msg: '删除菜单成功'
+      }))
+    })
+    .catch(err => {
+      res.json({
+        code: 400,
+        msg: err
+      })
+    })
+  // res.send('delete123')
 })
 
 module.exports = router;
